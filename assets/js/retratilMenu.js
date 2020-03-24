679a6a833b36cd9968eb6291e929468aeeea002e
warlockzeo <warlockzeo@gmail.com>
2020-03-24 21:20:53 +0000

    update styles on retratil menu

--- assets/js/retratilMenu.js
+++ assets/js/retratilMenu.js
@@ -1,8 +1,16 @@
 //menu retratil
 $(window).bind('scroll', function () {
-	if ($(window).scrollTop() < 150) {
-		$('nav.retratil').css({'top':'-65px'});
-	} else {
-		$('nav.retratil').css({'top':'0'});
-	}
+  if ($(window).scrollTop() < 150) {
+    $('.nav.retratil').css({ top: '-65px' });
+  } else {
+    $('.nav.retratil').css({ top: '0' });
+  }
+});
+
+$('.navbar-toggle').click(function (event) {
+  $('.navbar-collapse').toggleClass('collapsed');
+});
+
+$('.navbar-collapse').click(function (event) {
+  $('.navbar-collapse').removeClass('collapsed');
 });
