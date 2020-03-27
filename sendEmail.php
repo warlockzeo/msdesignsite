442d01ccfe4c6af5eda0994f463b4ad04425d878
warlockzeo <warlockzeo@gmail.com>
2020-03-27 21:40:51 +0000

    Update contact form

--- sendEmail.php
+++ sendEmail.php
@@ -3,13 +3,26 @@
         #exibir lista completa de Alunos
         public function enviarEmail($s, $m, $t)
         {
+            $m = "
+            <style>* {color: #000000;}</style>
+            $m
+            <br>
+            <br>
+            <br>
+            <p>
+            <img src='https://www.msdesign.pt/assets/images/logo2.png' alt='MSDesign' style='height: 65px;width: auto;float: left;padding-right: 20px;border-right: 3px solid #000000;margin-right: 20px;'>
+            <br>
+            Tlm / Whatsapp: +351 934 290 403
+            <br>
+            Facebook: fb/mariosilvadesign
+            </p>
+            ";
             $headers = "From: MSDesign.pt <contato@msdesign.pt>\r\n";
             $headers .= "Reply-To: $t\r\n";
             $headers .= "Content-type: text/html; charset=UTF-8\r\n";
 
             //Enviando o email
             if(mail($t, $s, $m, $headers)){
-                header("HTTP/1.0 200 OK");
                 return 'ok';
             } else {
                 header("HTTP/1.0 400 Falha no envio");
@@ -24,18 +37,29 @@
     //Recebendo dados do form
     $json = file_get_contents('php://input');
     $obj = json_decode($json, TRUE);
-    $nome = isset($_POST['nome']) ? preg_replace('/[^[:alpha:]_]/', '',$_POST['nome']): '';
-    $email = isset($_POST['email']) ? preg_replace('/[^[:alpha:]_]/', '',$_POST['email']): '';
-    $telefone = isset($_POST['telefone']) ? preg_replace('/[^[:alpha:]_]/', '',$_POST['telefone']): '';
-    $mensagem = isset($_POST['mensagem']) ? preg_replace('/[^[:alpha:]_]/', '',$_POST['mensagem']): '';
 
-    $msg = "\nNome: $nome \n\n$telefone \n\n$email \n\n$mensagem\n";
+    $nome = isset($obj['nome']) ? preg_replace('/[^[:alpha:][:space:]_]/', '',$obj['nome']): '';
+    $email = isset($obj['email']) ? preg_replace('/[^[:alnum:]@._]/', '',$obj['email']): '';
+    $fone = isset($obj['fone']) ? preg_replace('/[^[:alnum:]()-._]/', '',$obj['fone']): '';
+    $mensagem = isset($obj['mensagem']) ? preg_replace('/[^[:alnum:][:punct:][:space:]]/', '',$obj['mensagem']): '';
+
+    $msg = "Nome: $nome 
+    <br>
+    Telefone: $fone 
+    <br>
+    E-mail: $email 
+    <br>
+    Mensagem: $mensagem";
+
     $subject = "Mensagem de MSDesign.pt";
     $to = 'contact@msdesign.pt';
-  
     
-    if($mail1->enviarEmail($subject, $msg, $to)){
-        $msg = "Olá $nome\n\nNós do MSDesign agradecemos o seu contacto e estaremos entrando em contacto o mais rápido possível.";
-        echo($mail2->enviarEmail($subject, $msg, $email));
+    if($mail1->enviarEmail($subject, $msg, $to) === "ok"){
+        header("HTTP/1.0 200 OK");
+        $msg = "Olá $nome
+        <br >
+        Nós do <strong>MSDesign</strong> agradecemos o seu contacto e estaremos entrando em contacto o mais rápido possível.";
+
+        $mail2->enviarEmail($subject, $msg, $email);
     }
 ?>
